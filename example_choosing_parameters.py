eb099b77ef63ce62b4aa07c2cbcbacc480b57b3b
Hussein Saafan <hsaafan@uwaterloo.ca>
2022-05-04 20:46:51 -0400

    Added case study of idv1 Cleaned up code

--- example_choosing_parameters.py
+++ example_choosing_parameters.py
@@ -14,6 +14,7 @@ from math import floor
 
 
 def upper_quantile_x_speed(X: np.ndarray, q: float) -> float:
+    """Find the speed at which to split features into slow and fast"""
     m, n = X.shape
 
     X_dot = X[:, 1:] - X[:, :-1]
@@ -23,32 +24,68 @@ def upper_quantile_x_speed(X: np.ndarray, q: float) -> float:
     return(ordered_speeds[speed_index])
 
 
-if __name__ == "__main__":
-    speed_kept = 0.9
-    var_kept = 0.9
-    alpha = 0.01
-    degenerate_speed = 1e-10  # Below this speed, SF are considered degenrate
-    lag_range = [0, 100]  # [Min, Max] lags to test
-    dataset = 'TEP'       # ['TEP', 'CVA']
+def calculate_Md(Y: np.ndarray, n: int, upper_speed: float) -> int:
+    """Calculates the number of features to keep for SFA based algorithms"""
+    Y_dot = Y[:, 1:] - Y[:, :-1]
+    speeds = np.sum(Y_dot ** 2, axis=1) / (n - 1)
+    return(int(np.argwhere(np.sort(speeds) > upper_speed)[0]))
 
-    max_lag = [-1, -1, -1, -1]
-    FAR = [[], [], [], []]
-    results = []
-    for lag in range(lag_range[0], lag_range[1] + 1):
-        lag_result = [lag, 0, 0, 0, 0]
-        print(f"Testing lag = {lag}")
 
-        """Import Data"""
-        if dataset == 'TEP':
-            X, T0, _, _, _ = tepimport.import_tep_sets(lagged_samples=lag)
-        elif dataset == 'CVA':
-            data_sets_unlagged = cva.import_sets(lagged_samples=0)
-            X = np.hstack([x[1] for x in data_sets_unlagged[1:3]])
-            T0 = data_sets_unlagged[0][1]
-            X = X[:-1, :]    # Remove var 24
-            T0 = T0[:-1, :]  # Remove var 24
-            X = cva.add_lagged_samples(X, lagged_samples=lag)
-            T0 = cva.add_lagged_samples(T0, lagged_samples=lag)
+def message(msg: str, print_msg: bool) -> None:
+    """Helper function for cleaner looking code"""
+    if print_msg:
+        print(msg)
+
+
+def calculate_T2(Y: np.ndarray, gamma_inv: np.ndarray = None) -> np.ndarray:
+    """Calculate the T^2 statistic"""
+    n = Y.shape[1]
+    T2 = np.zeros((n, ))
+    if gamma_inv is None:
+        for i in range(n):
+            T2[i] = Y[:, i].T @ Y[:, i]
+    else:
+        Y = Y.T
+        for i in range(n):
+            T2[i] = Y[i, :] @ gamma_inv @ Y[i, :].T
+    return(T2)
+
+
+def calculate_FAR_FDR(Y_V: np.ndarray, Y_T: np.ndarray,
+                      T2c: float, fault_indices: list,
+                      gamma_inv: np.ndarray = None) -> tuple:
+    """Calculate FAR and FDR values for a validation and test set"""
+    n_validation = Y_V.shape[1]
+    n_faults = len(fault_indices)
+    n_normal = Y_T.shape[1] - n_faults
+
+    validation_faults = np.sum(calculate_T2(Y_V, gamma_inv) > T2c)
+    marked_faults = calculate_T2(Y_T, gamma_inv) > T2c
+    true_faults = np.sum(marked_faults[fault_indices])
+    false_alarms = np.sum(np.delete(marked_faults, fault_indices))
+
+    FAR_V = validation_faults / n_validation
+    FDR_T = true_faults / n_faults
+    FAR_T = false_alarms / n_normal
+    return(FAR_V, FDR_T, FAR_T)
+
+
+def choose_parameters(training_set: np.ndarray, validation_set: np.ndarray,
+                      test_set: np.ndarray, fault_indices: list, min_lag: int,
+                      max_lag: int, speed_kept: float, variance_kept: float,
+                      alpha: float, skip_sfa: bool = False,
+                      skip_ssfa: bool = False, skip_mssfa: bool = False,
+                      skip_spca: bool = False, verbose: bool = True):
+    results_sfa = []
+    results_ssfa = []
+    results_mssfa = []
+    results_spca = []
+
+    for lag in range(min_lag, max_lag + 1):
+        message(f"Testing lag = {lag}", verbose)
+        X = tepimport.add_lagged_samples(training_set, lag)
+        V = tepimport.add_lagged_samples(validation_set, lag)
+        T = tepimport.add_lagged_samples(test_set, lag)
 
         """Preprocess Data"""
         m = X.shape[0]
@@ -57,129 +94,141 @@ if __name__ == "__main__":
         X = X - X_mean
         X_std = np.std(X, axis=1).reshape((-1, 1))
         X = X / X_std
+        V = (V - X_mean) / X_std
+        T = (T - X_mean) / X_std
+
         upper_speed = upper_quantile_x_speed(X, 1 - speed_kept)
-        T0 = (T0 - X_mean) / X_std
 
         """Train Models"""
-        if max_lag[0] < 0:
-            print('    Testing SFA')
+        if not skip_sfa:
+            message('    Testing SFA', verbose)
             sfa_object = sfa.SFA()
-            W_sfa, Omega_inv_sfa = sfa_object.run(X, m)
-            Y_sfa = (W_sfa.T @ X)
-            Y_sfa_dot = Y_sfa[:, 1:] - Y_sfa[:, :-1]
-            speeds_sfa = np.sum(Y_sfa_dot ** 2, axis=1) / (n - 1)
-            Md_sfa = int(np.argwhere(np.sort(speeds_sfa) > upper_speed)[0])
-            if np.any(speeds_sfa < degenerate_speed):
-                max_lag[0] = lag
+            W, _ = sfa_object.run(X, m)
+            Y = (W.T @ X)
+            Md = calculate_Md(Y, n, upper_speed)
+            Me = m - Md
 
-            W_sfa, Omega_inv_sfa = sfa_object.run(X, Md_sfa)
-            T_sfa = (W_sfa.T @ T0)
-            stats_sfa = fd.calculate_test_stats(T_sfa, Md_sfa, Omega_inv_sfa)
-            T2c, _, _, _ = fd.calculate_crit_values(n, Md_sfa, m - Md_sfa,
-                                                    alpha)
-            FAR[0].append(np.sum(stats_sfa[0] > T2c) / T0.shape[1])
-            lag_result[1] = Md_sfa
+            T2c = fd.calculate_crit_values(n, Md, Me, alpha)[0]
+            W, _ = sfa_object.run(X, Md)
+            Y_V = (W.T @ V)
+            Y_T = (W.T @ T)
 
-        if max_lag[1] < 0:
-            print('    Testing SSFA')
+            metrics = calculate_FAR_FDR(Y_V, Y_T, T2c, fault_indices)
+            results_sfa.append([Md, metrics[0], metrics[1], metrics[2]])
+
+        if not skip_ssfa:
+            message('    Testing SSFA', verbose)
             ssfa_object = ssfa.SSFA()
-            W_ssfa, Omega_inv_ssfa, _, _, _ = ssfa_object.run(X, m)
-            Lambda_inv_ssfa = np.linalg.pinv(W_ssfa.T @ W_ssfa)
-            Y_ssfa = scipy.linalg.sqrtm(Lambda_inv_ssfa) @ (W_ssfa.T @ X)
-            Y_ssfa_dot = Y_ssfa[:, 1:] - Y_ssfa[:, :-1]
-            speeds_ssfa = np.sum(Y_ssfa_dot ** 2, axis=1) / (n - 1)
-            Md_ssfa = int(np.argwhere(np.sort(speeds_ssfa) > upper_speed)[0])
-            if np.any(speeds_ssfa < degenerate_speed):
-                max_lag[1] = lag
+            W, _, _, _, _ = ssfa_object.run(X, m)
+            correction = scipy.linalg.sqrtm(np.linalg.pinv(W.T @ W))
+            Y = correction @ (W.T @ X)
+            Md = calculate_Md(Y, n, upper_speed)
+            Me = m - Md
 
-            W_ssfa, Omega_inv_ssfa, _, _, _ = ssfa_object.run(X, Md_ssfa)
-            Lambda_inv_ssfa = np.linalg.pinv(W_ssfa.T @ W_ssfa)
-            T_ssfa = (W_ssfa.T @ T0)
-            stats_ssfa = [[0] * T0.shape[1]]
-            for i in range(T0.shape[1]):
-                stats_ssfa[0][i] = (T_ssfa[:, i].T @ Lambda_inv_ssfa
-                                    @ T_ssfa[:, i])
-            T2c, _, _, _ = fd.calculate_crit_values(n, Md_ssfa, m - Md_ssfa,
-                                                    alpha)
-            FAR[1].append(np.sum(stats_ssfa[0] > T2c) / T0.shape[1])
-            lag_result[2] = Md_ssfa
+            T2c = fd.calculate_crit_values(n, Md, Me, alpha)[0]
+            W, _, _, _, _ = ssfa_object.run(X, Md)
+            correction = scipy.linalg.sqrtm(np.linalg.pinv(W.T @ W))
+            Y_V = correction @ (W.T @ V)
+            Y_T = correction @ (W.T @ T)
 
-        if max_lag[2] < 0:
-            print('    Testing MSSFA')
+            metrics = calculate_FAR_FDR(Y_V, Y_T, T2c, fault_indices)
+            results_ssfa.append([Md, metrics[0], metrics[1], metrics[2]])
+
+        if not skip_mssfa:
+            message('    Testing MSSFA', verbose)
             mssfa_object = mssfa.MSSFA("chol", "l1")
             try:
-                W_mssfa, Omega_inv_mssfa, _, _, _ = mssfa_object.run(X, m)
-                Y_mssfa = (W_mssfa.T @ X)
-                Y_mssfa_dot = Y_mssfa[:, 1:] - Y_mssfa[:, :-1]
-                speeds_mssfa = np.sum(Y_mssfa_dot ** 2, axis=1) / (n - 1)
-                Md_mssfa = int(np.argwhere(np.sort(speeds_mssfa)
-                                           > upper_speed)[0])
-                if np.any(speeds_mssfa < degenerate_speed):
-                    max_lag[2] = lag
+                W, _, _, _, _ = mssfa_object.run(X, m)
+                Y = (W.T @ X)
+                Md = calculate_Md(Y, n, upper_speed)
+                Me = m - Md
 
-                W_mssfa, Omega_inv_mssfa, _, _, _ = mssfa_object.run(X,
-                                                                     Md_mssfa)
-                T_mssfa = (W_mssfa.T @ T0)
-                stats_mssfa = fd.calculate_test_stats(T_mssfa, Md_mssfa,
-                                                      Omega_inv_mssfa)
-                T2c, _, _, _ = fd.calculate_crit_values(n, Md_mssfa,
-                                                        m - Md_mssfa, alpha)
-                FAR[2].append(np.sum(stats_mssfa[0] > T2c) / T0.shape[1])
-                lag_result[3] = Md_mssfa
+                T2c = fd.calculate_crit_values(n, Md, Me, alpha)[0]
+                W, _, _, _, _ = mssfa_object.run(X, Md)
+                Y_V = (W.T @ V)
+                Y_T = (W.T @ T)
+
+                metrics = calculate_FAR_FDR(Y_V, Y_T, T2c, fault_indices)
+                results_mssfa.append([Md, metrics[0], metrics[1], metrics[2]])
             except np.linalg.LinAlgError as e:
                 if str(e) == "Matrix is not positive definite":
                     # The covariance matrix is not positive definite meaning
                     # the cholesky retraction can't be used
-                    max_lag[2] = lag - 1
+                    skip_mssfa = True
                 else:
                     # For other linear algebra errors, reraise error
                     raise np.linalg.LinAlgError(str(e))
 
-        if max_lag[3] < 0:
-            print('    Testing SPCA')
+        if not skip_spca:
+            message('    Testing SPCA', verbose)
             total_var = np.sum(np.var(X, axis=1))
-            var_threshold = total_var * var_kept
+            var_threshold = total_var * variance_kept
             K_min = 1
             K_max = m
             while True:
+                # Search for correct K value
                 if K_max - K_min <= 3:
                     for i in range(K_min, K_max + 1):
                         spca = SparsePCA(n_components=i, max_iter=500,
                                          tol=1e-6)
-                        Y_spca = spca.fit_transform(X.T)
+                        Y = spca.fit_transform(X.T)
 
-                        if np.sum(np.var(Y_spca, axis=0)) >= var_threshold:
-                            K_spca = i
+                        if np.sum(np.var(Y, axis=0)) >= var_threshold:
+                            K = i
                             break
                     break
-                K_spca = floor((K_min + K_max) / 2)
-                spca = SparsePCA(n_components=K_spca, max_iter=500, tol=1e-6)
-                Y_spca = spca.fit_transform(X.T)
-                if np.sum(np.var(Y_spca, axis=0)) >= var_threshold:
-                    K_max = K_spca
+                K = floor((K_min + K_max) / 2)
+                spca = SparsePCA(n_components=K, max_iter=500, tol=1e-6)
+                Y = spca.fit_transform(X.T)
+                if np.sum(np.var(Y, axis=0)) >= var_threshold:
+                    K_max = K
                 else:
-                    K_min = K_spca
-            if np.linalg.matrix_rank(spca.components_) < m:
-                max_lag[3] = lag
+                    K_min = K
 
-            P = spca.components_.T
-            P_d = P[:, :K_spca]
-            P_e = P[:, K_spca:]
-            scores_d = X.T @ P_d
-            scores_e = X.T @ P_e
-            gamma_inv_d = np.linalg.inv(np.cov(scores_d.T))
-            gamma_inv_e = np.linalg.inv(np.cov(scores_e.T))
-            stats_spca = fd.calculate_test_stats_pca(T0.T, P, gamma_inv_d,
-                                                     gamma_inv_e, K_spca)
-            T2c, _, _, _ = fd.calculate_crit_values_pca(X.T, P, n, K_spca,
-                                                        m - K_spca, alpha)
-            FAR[3].append(np.sum(stats_spca[0] > T2c) / T0.shape[1])
-            lag_result[4] = Md_sfa
+            P = spca.components_.T[:, :K]
+            T2c = fd.calculate_crit_values_pca(X.T, P, n, K, m - K, alpha)[0]
 
-        results.append(lag_result)
-        if np.all(np.asarray(max_lag) > -1):
+            P_d = P[:, :K]
+            gamma_inv = np.linalg.inv(np.cov((X.T @ P_d).T))
+
+            scores_V = (V.T @ P_d).T
+            scores_T = (T.T @ P_d).T
+
+            metrics = calculate_FAR_FDR(scores_V, scores_T, T2c, fault_indices,
+                                        gamma_inv)
+            results_spca.append([K, metrics[0], metrics[1], metrics[2]])
+
+        if np.all(np.asarray([skip_sfa, skip_ssfa, skip_mssfa, skip_spca])):
             break
+        fault_indices = [i - 1 for i in fault_indices]
 
-    print(f'Max lags: {max_lag}')
-    print(FAR)
-    print(results)
+    return(results_sfa, results_ssfa, results_mssfa, results_spca)
+
+
+if __name__ == '__main__':
+    speed_kept = 0.9
+    var_kept = 0.9
+    alpha = 0.01
+    lag_range = [0, 11]  # [Min, Max] lags to test
+    dataset = 'TEP'      # ['TEP', 'CVA']
+    skip_sfa = False
+    skip_ssfa = False
+    skip_mssfa = False
+    skip_spca = False
+
+    """Import Data"""
+    if dataset == 'TEP':
+        X, T0, _, _, _ = tepimport.import_tep_sets(lagged_samples=0)
+        T1 = tepimport.import_sets(1, skip_training=True)[0][1]
+        T1 = np.delete(T1, list(range(22, 41)), axis=0)
+        fault_indices = np.asarray([i for i in range(160, 960)])
+    elif dataset == 'CVA':
+        data_sets_unlagged = cva.import_sets(lagged_samples=0)
+        X = np.hstack([x[1] for x in data_sets_unlagged[1:3]])
+        T0 = data_sets_unlagged[0][1]
+        X = X[:-1, :]    # Remove var 24
+        T0 = T0[:-1, :]  # Remove var 24
+
+    print(choose_parameters(X, T0, T1, fault_indices, lag_range[0],
+                            lag_range[1], speed_kept, var_kept, alpha,
+                            skip_sfa, skip_ssfa, skip_mssfa, skip_spca))
